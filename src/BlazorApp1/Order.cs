aaedabd7a7084898267aa7b5b237ab8282ad850e
Brad Irby <brad@bradirby.com>
2020-08-14 12:42:35 +0200

    Init Repo

---
+++ src/BlazorApp1/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1
+{
+    public class Order
+    {
+        public string OrderName  { get; set; }
+        public List<OrderLineItem> LineItems { get; set; }
+        public int NumLineItems => LineItems.Count;
+
+        public Order()
+        {
+            LineItems = new List<OrderLineItem>();
+        }
+
+        public void AddLineItem()
+        {
+            Console.WriteLine($"Adding line item ");
+            LineItems.Add(new OrderLineItem());
+            Console.WriteLine($"Now i have {LineItems.Count}");
+        }
+    }
+
+    public class OrderLineItem
+    {
+        public int Quantity { get; set; }
+        public string ItemName { get; set; }
+
+        public OrderLineItem()
+        {
+            Quantity = DateTime.Now.Second;
+            ItemName = DateTime.Now.ToString();
+        }
+    }
+}
