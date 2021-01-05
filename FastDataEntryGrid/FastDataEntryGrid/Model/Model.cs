0d4dc8d3e2d8f98b4577559ffcc05d6897ad11cc
Brad Irby <brad@bradirby.com>
2021-01-05 11:39:59 +0100

    Adding project for fast data entry grid

---
+++ FastDataEntryGrid/FastDataEntryGrid/Model/Model.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastDataEntryGrid
+{
+    public class GridDetailItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreateDate { get; set; }
+        public MyLookupCode LookupCode { get; set; }
+        public bool BooValue { get; set; }
+    }
+
+
+    public class GridDetailItemAdapter
+    {
+        public GridDetailItem Data { get; }
+        private List<MyLookupCode> LookupCache { get; set; }
+
+        public GridDetailItemAdapter(GridDetailItem data, List<MyLookupCode> lkCache)
+        {
+            Data = data;
+            LookupCache = lkCache;
+        }
+
+        public bool BoolVal
+        {
+            get => Data.BooValue;
+            set { Data.BooValue = value; }
+        }
+
+        public int Quantity
+        {
+            get => Data.Quantity;
+            set { Data.Quantity = value; }
+        }
+
+        public DateTime CreateDate
+        {
+            get => Data.CreateDate;
+            set { Data.CreateDate = value; }
+        }
+
+        public string LookupValue => Data.LookupCode.Value;
+            
+        public string Name
+        {
+            get => Data.Name;
+            set { Data.Name = value; }
+        }
+    }
+
+    public class MyLookupCode
+    {
+        public int Code { get; set; }
+        public string Value { get; set; }
+    }
+}
