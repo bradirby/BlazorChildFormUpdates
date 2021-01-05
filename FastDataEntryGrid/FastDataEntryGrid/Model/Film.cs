b7e5e71b56b4961ff3143b7b6f0955ca2a9792d4
Brad Irby <brad@bradirby.com>
2021-01-05 16:47:23 +0100

    adding typeahead sample

---
+++ FastDataEntryGrid/FastDataEntryGrid/Model/Film.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastDataEntryGrid
+{
+    public class Film
+    {
+        public int Year { get; set; }
+        public string Title { get; set; }
+    }
+}
