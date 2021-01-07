eb537621dfa68d90c013392dcd45f51b6dd9feba
Brad Irby <brad@bradirby.com>
2021-01-07 12:02:53 +0100

    adding prj from chris sainty

---
+++ CustomInputControls/Blazor.App/Shared/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Blazor.App.Shared
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class GuidPerson
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class IntPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+
+}
